25d66c615b3693227c9a249c35eb621ea1bbba4c
edsette <eduardosettte@hotmail.com>
2023-05-07 13:53:27 +0100

    funcao-converter

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import 'semantic-ui-css/semantic.min.css'
 import { Grid } from 'semantic-ui-react';
 import Conversor from "./components/Conversor";
 
-
-
 function App() {
 
 const gridStyle = {
   padding: "30px 30px"
 }
-
   return (
   <Grid style={gridStyle} columns={6} centered>
     <Grid.Row>
-      <Grid.Columns>
-        <Conversor></Conversor>
-      </Grid.Columns>
+      <Grid.Column>
+        <Conversor moedaOrigem="USD" moedaDestino="BRL"></Conversor>
+      </Grid.Column>
     </Grid.Row>
 
   </Grid>
--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Card} from 'semantic-ui-react'
+import React, {useState} from 'react';
+import {Button, Card, Form, Input} from 'semantic-ui-react'
 
-const Conversor = () =>  {
+const Conversor = ({moedaDestino, moedaOrigem}) =>  {
+
+  const [loading, setLoading] = useState(false)
+  const [valor, setValor] = useState(0)
+  const [cotacao, setCotacao] = useState(5.36)
+
+  const mudarValor = (e, {value}) => {
+    setValor(value)
+  }
+
   return (
     <Card>
+      <Card.Content>
+        <Card.Header>Conversor de Moeda</Card.Header>
+        <Card.Meta>Cotação Atual</Card.Meta>
+        <Card.Description>
+          <Form>
+            <Form.Field>
+              <label>{moedaOrigem}</label>
+              <Input type='text' placeholder='Valor' onChange={mudarValor} value={valor}></Input>
+            </Form.Field>
 
+
+            <Form.Field>
+              <label>{moedaDestino}</label>
+              <label>{(valor*cotacao).toFixed(2)}</label>
+            </Form.Field>
+
+          </Form>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <Button color='green' fluid>
+          Converter
+        </Button>
+      </Card.Content>
     </Card>
   )
 }
