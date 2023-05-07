51dce811dc290b19a9366856c7f7a9b490306e47
edsette <eduardosettte@hotmail.com>
2023-05-07 14:57:00 +0100

    finalizando-conversor

--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -3,13 +3,28 @@ import {Button, Card, Form, Input} from 'semantic-ui-react'
 
 const Conversor = ({moedaDestino, moedaOrigem}) =>  {
 
-  const [loading, setLoading] = useState(false)
-  const [valor, setValor] = useState(0)
-  const [cotacao, setCotacao] = useState(5.36)
+  const [loading, setLoading] = useState(false);
+  const [valor, setValor] = useState(0);
+  const [cotacao, setCotacao] = useState(0);
 
   const mudarValor = (e, {value}) => {
     setValor(value)
-  }
+  };
+
+    const convert = async function(){
+
+      setLoading(true);
+      const url = `https://api.freecurrencyapi.com/v1/latest?apikey=abCRry168G60uFnLXGW3Xxy6syD9pHp8e088QqFk&currencies=${moedaOrigem}%2C${moedaDestino}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      const resultdata = await result.data
+    
+      console.log(response);
+      console.log(result);
+      console.log(resultdata);
+      setLoading(false);
+      setCotacao(result[`{moedaOrigem}%2C${moedaDestino}`])
+    };
 
   return (
     <Card>
@@ -33,7 +48,7 @@ const Conversor = ({moedaDestino, moedaOrigem}) =>  {
         </Card.Description>
       </Card.Content>
       <Card.Content>
-        <Button color='green' fluid>
+        <Button color='green' fluid loading={loading} onClick={convert}>
           Converter
         </Button>
       </Card.Content>
